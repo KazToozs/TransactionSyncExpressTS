5ebd63d25c254069ecc6722f3bb82a017efdb799
Will <cris.hobson@hotmail.com>
2021-08-22 17:59:18 +0200

    Completed algorithm; must refine unit tests

--- src/api/movements/validation/validation.controller.ts
+++ src/api/movements/validation/validation.controller.ts
@@ -15,44 +15,111 @@ class ValidationController {
         this.router.post(this.path, this.validateStatements);
     }
 
-    private removeDuplicateTransaction() {
-
-    }
-
-    private createMissingTransaction() {
-
-    }
-
     validateStatements = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
-        const transactions: Transaction[] = request.body["movements"];
+        let transactions: Transaction[] = request.body["movements"];
         const inspectionPoints: InspectionPoint[] = request.body["balances"];
+        // Probable point for code clarity: initialize date objects within these lists instead of doing it every time I need a check
+        // Need feedback for this
 
-        console.log(transactions, inspectionPoints) // TODO remove
-
-        // Promise really necessary given context? 
-        // Maybe not, but useful as a demonstration in the case in a real context we have to make an async call somewhere for this op
         try {
+            // Sort the lists to have the dates in order for analysis (if necessary)
+            transactions.sort((a, b) => { return -(new Date(b.date).getTime() - new Date(a.date).getTime()) })
+            inspectionPoints.sort((a, b) => { return -(new Date(b.date).getTime() - new Date(a.date).getTime()) })
+
+            // Promise really necessary given context? 
+            // Maybe not, but useful as a demonstration in the case in a real context we have to make an async call somewhere for this op
             let resList: Object[] = await new Promise((resolve, reject) => {
-                let reasons = null
-                // Sort the lists by date if necessary?
-                transactions.sort((a, b) => { return -(new Date(b.date).getTime() - new Date(a.date).getTime()) })
-                inspectionPoints.sort((a, b) => { return -(new Date(b.date).getTime() - new Date(a.date).getTime()) })
+                let reasons: Object[] = []
 
-                console.log(transactions, inspectionPoints) // TODO remove
+                // OF NOTE: The following code sections could be placed as private functions as necessary.
+                // I am prefering to keep them here as to clearly explain my code all in one place
 
-                inspectionPoints.forEach((inspectionPoint) => {
-                    console.log(inspectionPoint.balance)
-                })
+                // Remove any duplicates that can be immediately recognized
+                // I am basing my understanding of a 'duplicate' on the id alone. I am sure this might have to be more complicated... taking into account the other fields
+                // In which case a comparison between transaction dates/values/labels will be required
+                const onlyUnique = (value, index, self) => {
+                    // (self.findIndex) : finds the first index where the value 'id' is that of the item we are evaluating in the filter 
+                    // (index === self.find.index) : if the found index is not the same as our current filter object, it means it's a duplicate
+                    let isUnique = index === self.findIndex((elem) => (
+                        elem.id === value.id
+                    ))
+                    if (!isUnique) {
+                        const reason = {
+                            "problemType": "Duplicate Transaction",
+                            "transactionDate": value.date,
+                            "transactionAmount": value.amount,
+                            "actionRequired": "None"
+                        }
+                        reasons.push(reason)
+                    }
+                    return isUnique
+                }
+                transactions = transactions.filter(onlyUnique)
+
+                // We must also ignore any transaction prior to our first control point as it gives us no insight to the balance of the account
+                // Here I'm removing if equal to the date as well for simplicity sake, this may provoke a problem?
+                while (transactions.length > 0 &&
+                    new Date(transactions[0].date).getTime() <= new Date(inspectionPoints[0].date).getTime()) {
+                    transactions.shift()
+                }
+                // If all transactions take place before first period, send no problem
+                if (!transactions.length) {
+                    resolve(reasons)
+                    return
+                }
+                
+                // We need a baseline balance at start to know whether balances from following inspection points seem correct from the transactions that follow
+                // We take as first baseline balance the date of the first inspection point before the first transaction that we can have a baseline for
+                for (var inspectionStartIndex = 0;
+                    inspectionStartIndex < inspectionPoints.length - 1 &&
+                    new Date(inspectionPoints[inspectionStartIndex].date).getTime() < new Date(transactions[0].date).getTime();
+                    inspectionStartIndex++);
+                // in the case all transactions happen after the last inspection point, we suppose they are valid as we cannot know until next period
+                if (new Date(inspectionPoints[inspectionStartIndex].date).getTime() < new Date(transactions[0].date).getTime()) {
+                    resolve(reasons)
+                    return
+                }
+                // we come back one index to be on the inspection point just before our first analysable transaction
+                inspectionStartIndex--
+
+                // We then analyse if we obtain the correct balance with the given transactions per period between inspection points
+                let i = 0
+                let currentBalance = inspectionPoints[inspectionStartIndex].balance
+                while (inspectionStartIndex < inspectionPoints.length - 1) {
+                    while (i < transactions.length &&
+                        new Date(transactions[i].date).getTime() <= new Date(inspectionPoints[inspectionStartIndex + 1].date).getTime()) {
+                        currentBalance += transactions[i].amount
+                        i++;
+                    }
+                    // Upon completing analysis of transactions within a period, move to next period and set the balance to expected number for next period analysis
+                    inspectionStartIndex++
+                    // After adding transactions together for current period, check if there is a discrepancy
+                    // If so, add to reason list
+                    if (currentBalance != inspectionPoints[inspectionStartIndex].balance) {
+                        const reason = {
+                            "problemType": "Balance discrepency: likely missing transactions",
+                            "periodStart": inspectionPoints[inspectionStartIndex - 1].date,
+                            "periodEnd": inspectionPoints[inspectionStartIndex].date,
+                            "amountStart": inspectionPoints[inspectionStartIndex - 1].balance,
+                            "amountEndFound": currentBalance,
+                            "amountEndExpected": inspectionPoints[inspectionStartIndex].balance,
+                            "actionRequired": "Look for missing transactions"
+                        }
+                        reasons.push(reason)
+                        currentBalance = inspectionPoints[inspectionStartIndex].balance
+                    }
+                }
 
                 resolve(reasons)
             })
-            if (resList == null) {
+            if (resList.length == 0) {
                 response.status(202).send({ message: "Accepted" });
             } else {
                 response.status(418).send({ message: "I'm a teapot", "reasons": resList });
             }
         } catch (err) {
             // Maybe display error here
+            console.log(err) // TODO remove
             next(new HttpException(500, "Couldn't validate bank statements. Bad input formatting?"))
         }
 
---
+++ tests/app.test.ts
@@ -0,0 +1,277 @@
+import { expect } from 'chai';
+import * as request from 'supertest'
+import App from '../src/app'
+import ValidationController from '../src/api/movements/validation/validation.controller'
+
+describe('server', function () {
+    let app
+
+    before(function () {
+        app = new App(
+            [
+                new ValidationController(),
+            ],
+            5000, // Ideally, the port would be dynamic or set as an environment variable
+        );
+        // Probably use another port for testing aswell?
+    })
+    beforeEach(function (done) {
+        app.listen(function (err) {
+            if (err) { return done(err); }
+            done();
+        });
+    })
+    afterEach(function (done) {
+        app.server.close(done);
+    });
+
+
+    it('says Accepted when there are no problems', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // -> 300
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1300
+                {"id": 102, "date": "2010-02-17T12:00:00", "label": "shopping", "amount": -800}, // -> 500
+                {"id": 103, "date": "2010-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 1500
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1000},
+                {"date": "2010-03-01T12:00:00", "balance": 1500}
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(202, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    // Likely not a case that would be seen in real circumstances
+    it('says Accepted when dates are not relevant and there seems to be no problem', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2011-02-05T12:00:00", "label": "loyer", "amount": -700}, 
+                {"id": 101, "date": "2011-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, 
+                {"id": 102, "date": "2011-02-17T12:00:00", "label": "shopping", "amount": -800},
+                {"id": 103, "date": "2011-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, 
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1000},
+                {"date": "2010-03-01T12:00:00", "balance": 1500}
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(202, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    it('handles a missing transaction', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // -> 300
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1300
+                {"id": 103, "date": "2010-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 2300, missing -800 transactions
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1000},
+                {"date": "2010-03-01T12:00:00", "balance": 1500},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    it('handles a set of missing transactions', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // -> 300
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1300
+                {"id": 105, "date": "2010-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 2300, missing -800 transactions
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1000},
+                {"date": "2010-03-01T12:00:00", "balance": 1500},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    it('handles irrelevant transactions before first inspection point', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // -> 300
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1300
+                {"id": 105, "date": "2010-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 2300, missing -800 transactions
+            ],
+            "balances": [
+                {"date": "2010-03-01T12:00:00", "balance": 1500},
+                {"date": "2010-04-01T12:00:00", "balance": 2000},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(202, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+    it('handles a duplicate transaction', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // 800
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // 1800
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000},
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1500},
+                {"date": "2010-03-01T12:00:00", "balance": 1800},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+    it('handles a duplicate and a missing transaction', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // 800
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // 1800
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, 
+            ],
+            "balances": [
+                {"date": "2010-02-01T12:00:00", "balance": 1500},
+                {"date": "2010-03-01T12:00:00", "balance": 2800},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    it('handles a set of duplicate transactions', function (done) {
+        const data = {
+            "movements": [
+                {"id": 106, "date": "2010-03-05T12:00:00", "label": "loyer", "amount": -700}, // -> 800
+                {"id": 106, "date": "2010-03-05T12:00:00", "label": "loyer", "amount": -700}, // -> 100, duplicate, real 800
+                {"id": 106, "date": "2010-03-05T12:00:00", "label": "loyer", "amount": -700}, // -> -600, duplicate, real 800
+                {"id": 107, "date": "2010-03-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 400, real 1800
+                {"id": 108, "date": "2010-03-17T12:00:00", "label": "shopping", "amount": -200}, // -> 200, real 1600
+                {"id": 109, "date": "2010-03-18T12:00:00", "label": "shopping", "amount": -200}, // -> 0, real 1400
+                {"id": 110, "date": "2010-03-19T12:00:00", "label": "shopping", "amount": -200}, // -> -200, real 1200
+                {"id": 111, "date": "2010-03-20T12:00:00", "label": "shopping", "amount": -200}, // -> -400, real 1000
+                {"id": 112, "date": "2010-03-31T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 600, real 2000
+            ],
+            "balances": [
+                {"date": "2010-03-01T12:00:00", "balance": 1500},
+                {"date": "2010-04-01T12:00:00", "balance": 2000},
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+
+
+    it('handles long chains of transaction problems', function (done) {
+        const data = {
+            "movements": [
+                {"id": 100, "date": "2010-02-05T12:00:00", "label": "loyer", "amount": -700}, // -> 300
+                {"id": 101, "date": "2010-02-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1300
+                {"id": 105, "date": "2010-02-28T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 2300, missing -800 transactions
+                {"id": 106, "date": "2010-03-05T12:00:00", "label": "loyer", "amount": -700}, // -> 800
+                {"id": 106, "date": "2010-03-05T12:00:00", "label": "loyer", "amount": -700}, // -> 100, duplicate, real 800
+                {"id": 107, "date": "2010-03-15T12:00:00", "label": "revenu mi mois", "amount": 1000}, // -> 1100, real 1800
+                {"id": 108, "date": "2010-03-17T12:00:00", "label": "shopping", "amount": -200}, // -> 900, real 1600
+                {"id": 109, "date": "2010-03-18T12:00:00", "label": "shopping", "amount": -200}, // -> 700, real 1400
+                {"id": 110, "date": "2010-03-19T12:00:00", "label": "shopping", "amount": -200}, // -> 500, real 1200
+                {"id": 111, "date": "2010-03-20T12:00:00", "label": "shopping", "amount": -200}, // -> 300, real 1000
+                {"id": 112, "date": "2010-03-31T12:00:00", "label": "revenu fin mois", "amount": 1000}, // -> 1300, real 2000
+            ],
+            "balances": [
+                {"date": "2010-01-01T12:00:00", "balance": 800},
+                {"date": "2010-02-01T12:00:00", "balance": 1000},
+                {"date": "2010-03-01T12:00:00", "balance": 1500},
+                {"date": "2010-04-01T12:00:00", "balance": 2000},
+                {"date": "2010-05-01T12:00:00", "balance": 1800},
+                // {"date": "2010-06-01T12:00:00", "balance": 2500},
+                // {"date": "2010-07-01T12:00:00", "balance": 1000},
+                // {"date": "2010-08-01T12:00:00", "balance": 1500},
+                // {"date": "2010-09-01T12:00:00", "balance": 2000},
+                // {"date": "2010-10-01T12:00:00", "balance": 1700},
+                // {"date": "2010-11-01T12:00:00", "balance": 2300},
+                // {"date": "2010-12-01T12:00:00", "balance": 2500}
+            ]
+        }
+
+        request(app.app)
+            .post('/movements/validation')
+            .set('Content-Type', 'application/json')
+            .send(data)
+            .expect(418, function (err, res) {
+                if (err) { return done(err); }
+                // Done
+                done();
+            });
+    })
+})
